f65d4ec3c3e8cefdf3a3fde90789e674087384de
Anthony <130808394+anthonyfamar@users.noreply.github.com>
2026-03-29 19:49:10 -0400

    função editar e deletar pessoa

--- src/backend/GFR.Api/Controllers/PessoasController.cs
+++ src/backend/GFR.Api/Controllers/PessoasController.cs
@@ -15,6 +15,7 @@ namespace GFR.Api.Controllers
             _context = context;
         }
 
+
         [HttpPost]
         public IActionResult Criar(Pessoa pessoa)
         {
@@ -24,11 +25,51 @@ namespace GFR.Api.Controllers
             return Ok("Pessoa registrada com sucesso.");
         }
 
+        [HttpPut("{id}")]
+        public IActionResult Editar(int id, Pessoa pessoaEditada)
+        {
+            var pessoa = ObterPessoa(id);
+
+            if (pessoa == null)
+                return NotFound("Pessoa não encontrada");
+
+            pessoa.Nome = pessoaEditada.Nome;
+            pessoa.Idade = pessoaEditada.Idade;
+
+            _context.SaveChanges();
+
+            return Ok(pessoa);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            var pessoa = ObterPessoa(id);
+
+            if (pessoa == null)
+                return NotFound("Pessoa não encontrada.");
+           
+            //remove as transações da pessoas em especifico
+            var transacoes = _context.Transacoes.Where(t => t.PessoaId == id).ToList(); 
+            _context.Transacoes.RemoveRange(transacoes);
+
+            //remove a pessoa
+            _context.Pessoas.Remove(pessoa);
+
+            _context.SaveChanges();
+            return Ok("Pessoa e suas transações removidas com sucesso.");
+        }
+
         [HttpGet]
         public IActionResult Listar() 
         {
             var pessoas = _context.Pessoas.ToList();
             return Ok(pessoas);
+        }
+
+        private Pessoa? ObterPessoa(int id)
+        {
+            return _context.Pessoas.Find(id);
         }
     }
 }
